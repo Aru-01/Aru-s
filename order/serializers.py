db011c44ffaeb1a4d95bcd124357dd4cdb78a0ca
arifu00 <arifu5758@gmail.com>
2025-12-02 20:59:56 +0600

    update & imp: [nesting cart & cartitem serialize to show cart Items & items detail & total Price

--- order/serializers.py
+++ order/serializers.py
@@ -1,8 +1,33 @@
 from rest_framework import serializers
 from order.models import Cart, CartItem, Order, OrderItem
+from product.models import Product
+
+
+class SimpleProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "name", "price"]
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product = SimpleProductSerializer()
+    total_price = serializers.SerializerMethodField("get_total_price")
+
+    class Meta:
+        model = CartItem
+        fields = ["id", "product", "quantity", "total_price"]
+
+    def get_total_price(self, cart_item: CartItem):
+        return cart_item.quantity * cart_item.product.price
 
 
 class CartSerialzer(serializers.ModelSerializer):
+    items = CartItemSerializer(many=True)
+    total_price = serializers.SerializerMethodField("get_total_price")
+
     class Meta:
         model = Cart
-        fields = ["id", "user"]
+        fields = ["id", "user", "items", "total_price"]
+
+    def get_total_price(self, cart: Cart):
+        return sum([item.quantity * item.product.price for item in cart.items.all()])
--- product/category_urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from product import views
-
-urlpatterns = [
-    path("", views.CategoryList.as_view(), name="category-list"),
-    path("<int:id>/", views.CategoryDetails.as_view(), name="category-Details"),
-]
